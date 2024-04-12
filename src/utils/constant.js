153e220655265a0f6ab3216c3d621b74002a4030
lokesh maurya <lokeshmaurya010@gmail.com>
2024-04-13 01:56:16 +0530

    minor commit

---
+++ src/utils/constant.js
@@ -0,0 +1,23 @@
+export const USER_AVATAR =
+  "https://avatars.githubusercontent.com/u/126377788?v=4";
+export const BG_IMAGE =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjUxZDk1ZWNmYWM4ZTViYjBlZDA3YjkwYTRiNDViYSIsInN1YiI6IjY1ZjMzNjA1OTkyNTljMDE4NjYxMGVlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C8NN9w3akoDR7igeBauIAet5Vr9rfWMOk0meO7JOtMA",
+  },
+};
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780";
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", language: "English" },
+  { identifier: "hindi", language: "Hindi" },
+  { identifier: "spanish", language: "Spanish" },
+];
+
+export const OPENAI_KEY_1 =
+  "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";
+export const OPENAI_KEY_2 =
+  "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";
